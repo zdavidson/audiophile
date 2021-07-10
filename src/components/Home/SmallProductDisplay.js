f886b3a2e53c132750e9bb9aaad3864fcca693a9
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-10 13:22:15 -0400

    small product styled

--- src/components/Home/SmallProductDisplay.js
+++ src/components/Home/SmallProductDisplay.js
@@ -1,5 +1,17 @@
+import "./SmallProductDisplay.scss";
+
 const SmallProductDisplay = () => {
-  return <div>Displaying</div>;
+  return (
+    <div id="small-product-home">
+      <div id="small-product-image">
+        <img src="./assets/home/desktop/image-earphones-yx1.jpg" alt="" />
+      </div>
+      <div id="small-product-text">
+        <h2>YX1 Earphones</h2>
+        <button>See Product</button>
+      </div>
+    </div>
+  );
 };
 
 export default SmallProductDisplay;
