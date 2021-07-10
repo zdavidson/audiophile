4645ac6c6802e045a9b3f4a3dd73bf7c2a3e63af
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-10 13:11:22 -0400

    medium product display added

--- src/components/Home/MediumProductDisplay.js
+++ src/components/Home/MediumProductDisplay.js
@@ -1,5 +1,14 @@
+import "./MediumProductDisplay.scss";
+
 const MediumProductDisplay = () => {
-  return <div>Displaying</div>;
+  return (
+    <div id="medium-product-home">
+      <div id="medium-product-text">
+        <h2>ZX7 Speaker</h2>
+        <button>See Product</button>
+      </div>
+    </div>
+  );
 };
 
 export default MediumProductDisplay;
